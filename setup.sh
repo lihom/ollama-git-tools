1ab6c44dd6257f5862b0195f6659c714d4949b11
hom <chiangri0713@gmail.com>
2025-12-25 15:26:26 +0800

    add dist path for setup script link.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # setup.sh
 
+read -p "please enter your git root path: " GIT_ROOT_PATH
+
 echo "Installing Ollama Git hooks..."
 
 # Create symlinks from the repo's hooks folder to the actual git hooks folder
-ln -sf ./hooks/pre-commit .git/hooks/pre-commit
-ln -sf ./hooks/prepare-commit-msg .git/hooks/prepare-commit-msg
+ln -sf ./hooks/pre-commit $GIT_ROOT_PATH/.git/hooks/pre-commit
+ln -sf ./hooks/prepare-commit-msg $GIT_ROOT_PATH/.git/hooks/prepare-commit-msg
 
 # Ensure they are executable
 chmod +x hooks/*
